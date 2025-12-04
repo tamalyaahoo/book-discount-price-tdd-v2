78508a61c8f04e75514fa08e1c084d8e613066fb
TAMAL NAYEK <tamal.nayek@yahoo.com>
2025-12-05 02:52:26 +0530

    refactor: Replace BookEnum fields with record-based structure and extract author constants.
    Remove duplicate author literals and introduce constants in record-based BookEnum
    
    Closes #5

---
+++ src/main/java/com/bnpp/kata/book/price/store/Author.java
@@ -0,0 +1,17 @@
+package com.bnpp.kata.book.price.store;
+
+public enum Author {
+    RM("Robert Martin"),
+    KB("Kent Beck"),
+    MF("Michael Feathers");
+
+    private final String fullName;
+
+    Author(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
---
+++ src/main/java/com/bnpp/kata/book/price/store/BookInfo.java
@@ -0,0 +1,10 @@
+package com.bnpp.kata.book.price.store;
+
+public interface BookInfo {
+
+    int id();
+    String title();
+    String author();
+    int year();
+    double price();
+}
