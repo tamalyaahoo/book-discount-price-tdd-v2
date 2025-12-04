6252d051ff20a8c65643405ece31c66cc98abe49
TAMAL NAYEK <tamal.nayek@yahoo.com>
2025-12-05 04:19:15 +0530

    docs: Add Swagger/OpenAPI documentation.
    Add pom dependency and update controller file OpenAPI documentation.
    
    Closes #10

--- src/main/java/com/bnpp/kata/book/price/controller/BookController.java
+++ src/main/java/com/bnpp/kata/book/price/controller/BookController.java
@@ -2,6 +2,8 @@ package com.bnpp.kata.book.price.controller;
 
 import com.bnpp.kata.book.price.dto.BookResponse;
 import com.bnpp.kata.book.price.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@Tag(name = "Book API", description = "APIs for fetching development books")
 public class BookController {
 
     private final BookService bookService;
@@ -20,6 +23,7 @@ public class BookController {
     }
 
     @GetMapping("/getbooks")
+    @Operation(summary = "Fetch all books", description = "Returns all development books stored in enum")
     public ResponseEntity<List<BookResponse>> getAllBooks() {
         return ResponseEntity.ok(bookService.getAllBooks());
     }
